0a61034c6b878010b0ac4acfaa7f3ce0c118b17a
Minhhhh <mineskiroxor@gmail.com>
2023-08-27 02:43:02 +0800

    feat: add projects

---
+++ pages/1_💻_Projects.py
@@ -0,0 +1,36 @@
+import os
+import streamlit as st
+from utils.config import get_config
+from PIL import Image
+from streamlit_extras.badges import badge
+
+
+st.header("Personal Workspace")
+
+
+ASSET_DIR = f"{os.getcwd()}/assets"
+CONFIG = get_config()
+PAGE = "Projects"
+
+def get_project_names():
+    names = []
+    for i in CONFIG[PAGE]:
+        names.append(CONFIG[PAGE][i]["project_name"])
+    return names
+
+project_names = get_project_names()
+tabs = st.tabs(project_names)
+
+for idx, tab in enumerate(tabs):
+    with tab:
+        proj_details = CONFIG[PAGE][project_names[idx]]
+        st.image(Image.open(f"{ASSET_DIR}/{proj_details['image_path']}"))
+        with st.expander("Details", expanded=True):
+            badge(type="github",
+                  url=proj_details['repo_link'],
+                  name=proj_details['repo_name'])
+            info = f"{proj_details['description']} \n\n Technologies: {proj_details['tech_stack']}"
+            st.info(info, icon="🔥")
+
+        st.markdown("***")
+
